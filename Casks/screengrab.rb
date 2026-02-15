b85f32956c4aa75518cd06afc4b0d5fbb5392750
Dominic Slee <domslee1@gmail.com>
2026-02-15 13:52:41 +1100

    Update repo URLs from screen-grab to ScreenGrab

--- Casks/screengrab.rb
+++ Casks/screengrab.rb
@@ -2,10 +2,10 @@ cask "screengrab" do
   version "0.1.1-Beta"
   sha256 "c5faf19b397e62dddf2b7ddb4f8f12cc3f63955a528e41145679f0bb2a3f2b1b"
 
-  url "https://github.com/domsleee/screen-grab/releases/download/v0.1.1-Beta/ScreenGrab-v0.1.1-Beta.zip"
+  url "https://github.com/domsleee/ScreenGrab/releases/download/v0.1.1-Beta/ScreenGrab-v0.1.1-Beta.zip"
   name "ScreenGrab"
   desc "ShareX-like screen capture for macOS"
-  homepage "https://github.com/domsleee/screen-grab"
+  homepage "https://github.com/domsleee/ScreenGrab"
 
   depends_on macos: ">= :ventura"
 
